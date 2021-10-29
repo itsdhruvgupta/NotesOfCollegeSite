a2e2bfeeb6e9eb101d5238b592c11b2ca1646497
dhruv gupta <itsdhruvgupta1@gmail.com>
2021-10-29 23:44:00 +0530

    added courses page

--- notesSite/notesSite/urls.py
+++ notesSite/notesSite/urls.py
@@ -20,4 +20,5 @@ from notesSite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",views.index, name = "home"),
+    path("select-course/",views.select_course, name = "select_course"),
 ]
--- notesSite/notesSite/views.py
+++ notesSite/notesSite/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import redirect, render , HttpResponse
 
 def index(request):
     return render(request,'index.html')
+
+def select_course(request):
+    return render(request,'course.html')
+    
