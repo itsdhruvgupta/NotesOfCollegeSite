7c6ae601c19ce2eb4991add5a5b2450455c1f4d0
dhruv gupta <itsdhruvgupta1@gmail.com>
2022-02-06 16:55:24 +0530

    no message

--- notesSite/notesSite/settings.py
+++ notesSite/notesSite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-))5&e43tr7%k+)-f5&oe_irkkfiyc+cgj#s&iyw$2&27g(ffsj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.105']
+ALLOWED_HOSTS = ['192.168.0.105','127.0.0.1']
 
 
 # Application definition
